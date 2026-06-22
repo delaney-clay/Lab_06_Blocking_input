4bf927d1d3f1bbc1827da45949cb2ebf17a0580a
delaney-clay <claydk@mail.uc.edu>
2026-06-22 14:23:22 -0400

    before first run task 4 commit

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class HighorLow
@@ -5,6 +6,47 @@ public class HighorLow
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        Random generator = new Random();
+        int val = generator.nextInt(10)+1;
+        int guess = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("Guess a number between 1 and 10: ");
+            if(in.hasNextInt())
+            {
+                guess = in.nextInt();
+                if(guess < 1 && guess > 10)
+                {
+                    System.out.println("You entered " + guess + ". Please enter a number 1-10.");
+                    done = false;
+                }
+                else if(guess < val)
+                {
+                    System.out.println("Your guess was too low! The number was " + val + ".");
+                    System.out.println("Try again.");
+                    done = false;
+                }
+                else if(guess > val)
+                {
+                    System.out.println("Your guess was too high! The number was " + val + ".");
+                    System.out.println("Try again.");
+                    done = false;
+                }
+                else
+                {
+                    System.out.println("Correct!");
+                    done = true;
+                }
+            }
+            else
+            {
+                trash = in.next();
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        } while(!done);
 
     }
 
